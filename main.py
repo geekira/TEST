761bdafa8ff261e2cb7d8486af876aff55c05ad3
geekira <k-tonythe68@hotmail.fr>
2022-03-19 12:28:10 +0100

    Training

---
+++ main.py
@@ -0,0 +1,20 @@
+password = input()
+code = input()
+def creditmutuel(text1,text2):
+    if password == code:
+        print("Validate")
+    else:
+        print("Incorrect")
+
+creditmutuel(password, code)
+
+
+
+
+
+
+
+
+
+
+
---
+++ testt.py
@@ -0,0 +1,44 @@
+n = int(input())
+sum = 0
+length = 0
+
+while n > 0:
+    n //= 10
+    length += 1
+
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
