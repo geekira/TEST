75748fefabb6c5b4e37ebeda7ce3a558cccf37d0
geekira <k-tonythe68@hotmail.fr>
2022-03-24 11:28:14 +0100

    1 days

---
+++ Python/BOT .py
@@ -0,0 +1,14 @@
+import pyautogui, time
+
+time.sleep(10)
+pyautogui.click()
+for i in range(100):
+    pyautogui.write('Salut je suis un Bot lol')
+    pyautogui.press('Enter')
+    time.sleep(0.3)
+
+
+
+
+
+
